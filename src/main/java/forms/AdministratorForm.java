281178f709792c445793d295e02bd5cac8c99262
sergioperez1998 <sergio-pm@hotmail.com>
2019-03-03 23:28:11 +0100

    creada clase administrator form

--- src/main/java/forms/AdministratorForm.java
+++ src/main/java/forms/AdministratorForm.java
@@ -1,6 +1,89 @@
+
 package forms;
 
-
 public class AdministratorForm {
+
+	private String	username;
+	private String	password;
+	private String	name;
+	private String	middleName;
+	private String	surname;
+	private String	photo;
+	private String	email;
+	private String	phoneNumber;
+	private String	address;
+
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getMiddleName() {
+		return this.middleName;
+	}
+
+	public void setMiddleName(final String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(final String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhoto() {
+		return this.photo;
+	}
+
+	public void setPhoto(final String photo) {
+		this.photo = photo;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(final String address) {
+		this.address = address;
+	}
 
 }
