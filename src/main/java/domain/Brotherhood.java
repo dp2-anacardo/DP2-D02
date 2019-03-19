8fc45d17b13c9b4aa7542c543073807c8f3408fc
MrGaRRos <juanmagarrocho@gmail.com>
2019-03-19 18:06:11 +0100

    Anacardo 2.0

--- src/main/java/domain/Brotherhood.java
+++ src/main/java/domain/Brotherhood.java
@@ -35,13 +35,6 @@ public class Brotherhood extends Actor {
 	private Area			area;
 
 
-	//	private InceptionRecord				inceptionRecord;
-	//	private Collection<PeriodRecord>	periodsRecords;
-	//	private Collection<LegalRecord>		legalsRecords;
-	//	private Collection<MiscRecord>		miscsRecords;
-	//	private Collection<LinkRecord>		linksRecords;
-	//	private Collection<LinkRecord>		linkedsRecordsBrotherhood;
-
 	@NotBlank
 	@SafeHtml(whitelistType = WhiteListType.NONE)
 	public String getTitle() {
@@ -84,59 +77,5 @@ public class Brotherhood extends Actor {
 	public void setArea(final Area area) {
 		this.area = area;
 	}
-	//
-	//	@Valid
-	//	@OneToOne(optional = false, mappedBy = "brotherhood")
-	//	public InceptionRecord getInceptionRecord() {
-	//		return this.inceptionRecord;
-	//	}
-	//
-	//	public void setInceptionRecord(final InceptionRecord inceptionRecord) {
-	//		this.inceptionRecord = inceptionRecord;
-	//	}
-	//
-	//	@OneToMany(cascade = CascadeType.ALL, mappedBy = "brotherhood")
-	//	public Collection<PeriodRecord> getPeriodsRecords() {
-	//		return this.periodsRecords;
-	//	}
-	//
-	//	public void setPeriodsRecords(final Collection<PeriodRecord> periodsRecords) {
-	//		this.periodsRecords = periodsRecords;
-	//	}
-	//
-	//	@OneToMany(cascade = CascadeType.ALL, mappedBy = "brotherhood")
-	//	public Collection<LegalRecord> getLegalsRecords() {
-	//		return this.legalsRecords;
-	//	}
-	//
-	//	public void setLegalsRecords(final Collection<LegalRecord> legalsRecords) {
-	//		this.legalsRecords = legalsRecords;
-	//	}
-	//
-	//	@OneToMany(cascade = CascadeType.ALL, mappedBy = "brotherhood")
-	//	public Collection<MiscRecord> getMiscsRecords() {
-	//		return this.miscsRecords;
-	//	}
-	//
-	//	public void setMiscsRecords(final Collection<MiscRecord> miscsRecords) {
-	//		this.miscsRecords = miscsRecords;
-	//	}
-	//
-	//	@OneToMany(cascade = CascadeType.ALL, mappedBy = "brotherhood")
-	//	public Collection<LinkRecord> getLinksRecords() {
-	//		return this.linksRecords;
-	//	}
-	//
-	//	public void setLinksRecords(final Collection<LinkRecord> linksRecords) {
-	//		this.linksRecords = linksRecords;
-	//	}
-	//
-	//	@OneToMany(cascade = CascadeType.ALL, mappedBy = "linkedBH")
-	//	public Collection<LinkRecord> getLinkedsRecordsBrotherhood() {
-	//		return this.linkedsRecordsBrotherhood;
-	//	}
-	//
-	//	public void setLinkedsRecordsBrotherhood(final Collection<LinkRecord> linkedsRecordsBrotherhood) {
-	//		this.linkedsRecordsBrotherhood = linkedsRecordsBrotherhood;
-	//	}
+
 }
